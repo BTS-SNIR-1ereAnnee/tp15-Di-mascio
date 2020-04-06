2c21d948f1e7e6a486d4fe2fc39fda343ae86116
Di-mascio <di-mascio.esteban@lyceeppr.fr>
2020-04-06 17:09:06 +0200

    etapes 1

---
+++ javascript/testboutton.js
@@ -0,0 +1,18 @@
+var temp = 10;
+var hum = 10;
+var press = 10;
+
+function functionTemp()
+{
+document.getElementById("afficher").innerHTML = temp;
+}
+
+function functionHum()
+{
+document.getElementById("afficher").innerHTML = hum;
+}
+
+function functionPress()
+{
+document.getElementById("afficher").innerHTML = press;
+}
